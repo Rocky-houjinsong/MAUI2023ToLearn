7928893ba3f4b22e9600c572deff776fd34c6f6e
宋厚锦 <rocky.houjinsong@outlook.com>
2023-03-15 15:37:22 +0800

    对 MAUI 的基础使用 MVVM 架构 中View 如何同ViewModel进行建立联系;
    Signed-off-by: Rocky-houjinsong <rocky.houjinsong@outlook.com>

---
+++ HelloWorld01/HelloWorld/ServiceLocator.cs
@@ -0,0 +1,18 @@
+﻿using HelloWorld.ViewModels;
+
+namespace HelloWorld;
+
+public class ServiceLocator
+{
+    private IServiceProvider _serviceProvider;
+
+    public MainPageViewModel MainPageViewModel =>
+        _serviceProvider.GetService<MainPageViewModel>(); // 依赖注入; IoC
+
+    public ServiceLocator()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddSingleton<MainPageViewModel>();
+        _serviceProvider = serviceCollection.BuildServiceProvider();
+    }
+}
---
+++ HelloWorld01/HelloWorld/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace HelloWorld.ViewModels
+{
+    public class MainPageViewModel : ObservableObject
+    {
+        private string _result = string.Empty;
+
+        public string Result
+        {
+            get => _result;
+            set => SetProperty(ref _result, value);
+        }
+
+        private RelayCommand _clickMeCommand;
+
+        public RelayCommand ClickMeCommand =>
+            _clickMeCommand ??= new RelayCommand(() => Result = "Hello World! MAUI");
+    }
+}
